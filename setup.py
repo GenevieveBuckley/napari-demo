8bd13f4d0863ec164f2cebade5602faf4c5aea34
ziyang <zliu@chanzuckerbreg.com>
2021-02-18 13:44:34 -0800

    missing classifier

--- setup.py
+++ setup.py
@@ -38,19 +38,6 @@ setup(
     install_requires=requirements,
     use_scm_version=use_scm,
     setup_requires=['setuptools_scm'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Framework :: napari',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: BSD License',
-    ],
     entry_points={
         'napari.plugin': [
             'napari-demo = napari_demo',
